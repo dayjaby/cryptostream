72f06a8e3c7df0f3ac3415039d4ef81c2e7ef4cf
David Jablonski <dayjaby@gmail.com>
2018-07-31 13:17:29 +0200

    initial commit

---
+++ examples/encrypt-and-decrypt.py
@@ -0,0 +1,15 @@
+from cryptostream import CryptoStream
+
+encryptor = CryptoStream()
+encryptor.add_recipient("other company", open("other company.pub").read(), '127.0.0.1:8554', private_key = open("other company.priv").read())
+encryptor.add_recipient("sitebots", open("sitebots.pub").read(), '127.0.0.1:8555', private_key = open("sitebots.priv"))
+encryptor.encrypt(3*'a' + 17*'b')
+encryptor.encrypt(7*'c' + 7*'d')
+
+bdata = open("sitebots.bin", "rb").read()
+data = encryptor.decrypt(bdata)
+print(data)
+
+bdata = open("oc.bin", "rb").read()
+data = encryptor.decrypt(bdata, 1)
+print(data)
---
+++ examples/generate-keys.py
@@ -0,0 +1,4 @@
+from cryptostream import CryptoStream
+encryptor = CryptoStream()
+encryptor.generate("sitebots")
+encryptor.generate("other company")
